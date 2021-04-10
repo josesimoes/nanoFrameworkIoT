5629d89c1a5c24552b5206a70af47bff2eee3832
Laurent Ellerbach <laurelle@microsoft.com>
2021-04-10 14:10:40 +0300

    Add the static analyzer

---
+++ AnalyzeToFindElements/AnalyzeToFindElements/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AnalyzeToFindElements
+{
+    class Program
+    {
+        private static int _totalFilesAnalyzed = 0;
+        private static int _totalFilesWithPotentialIssues = 0;
+        private static int _totalFilesToBeTRansformed = 0;
+        private static int _totalBindings = 0;
+        private static int _totalBindingsWithPotentialIssues = 0;
+        private static int _totalTransformationNeeded = 0;
+        private static int _totalNumberUnitsNet = 0;
+        private static List<string> _units = new List<string>();
+        private static List<string> _unitsUsed = new List<string>();
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .NET IoT Analyzer!");
+
+            string pathToSearch = string.Empty;
+            string pathToUnitsNet = string.Empty;
+            if (args?.Length > 0)
+            {
+                pathToSearch = args[0];
+                if (args.Length > 1)
+                {
+                    pathToUnitsNet = args[1];
+                }
+            }
+
+            pathToSearch = string.IsNullOrEmpty(pathToSearch) ? @"C:\Repos\iot\src\devices" : pathToSearch;
+            pathToUnitsNet = string.IsNullOrEmpty(pathToUnitsNet) ? @"C:\Repos\UnitsNet\UnitsNet\GeneratedCode\Quantities" : pathToUnitsNet;
+
+            LoadAllUnits(pathToUnitsNet);
+
+            ProcessAllFiles(pathToSearch, true);
+
+            Console.WriteLine($"Total number of root folders (bindings): {_totalBindings}");
+            Console.WriteLine($"Total potential bindings which will need transformations: {_totalTransformationNeeded}");
+            Console.WriteLine($"Total potential bindings with potential issues (async, Linq and other simple Span<byte> transformation): {_totalBindingsWithPotentialIssues}");
+            Console.WriteLine($"Total number of files containing UnitsNet: {_totalNumberUnitsNet}");
+            _unitsUsed = _unitsUsed.Distinct().ToList();
+            foreach (var unit in _unitsUsed)
+            {
+                Console.WriteLine($"  {unit}");
+            }
+
+            Console.WriteLine($"Total files analyzed: {_totalFilesAnalyzed}");
+            Console.WriteLine($"Total files with potential issues: {_totalFilesWithPotentialIssues}");
+        }
+
+        private static void LoadAllUnits(string pathToUnitsNet)
+        {
+            foreach (var file in Directory.GetFiles(pathToUnitsNet, "*.g.cs"))
+            {
+                string nameOnly = file.Substring(file.LastIndexOf("\\") + 1);
+                nameOnly = nameOnly.Substring(0, nameOnly.Length - 5);
+                _units.Add(nameOnly);
+            }
+        }
+
+        private static (bool issue, bool transofmration) ProcessAllFiles(string directory, bool isRoot = false)
+        {
+            bool issuesInDirectory = false;
+            bool transformationNeeded = false;
+
+            foreach (var file in Directory.GetFiles(directory, "*.cs"))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    bool somethingFound = false;
+                    var textFile = reader.ReadToEnd();
+
+                    var result = SearchForSpanListT(textFile);
+
+                    if (result.Count > 0)
+                    {
+                        Console.WriteLine($"{file}");
+                        var distincts = result.Select(m => m.Groups[0].Value).Distinct();
+                        foreach (var res in distincts)
+                        {
+                            if (res.Contains("Span<byte>"))
+                            {
+                                transformationNeeded = true;
+                            }
+                            else
+                            {
+                                somethingFound = true;
+                            }
+
+                            Console.WriteLine($"  {res} - count: {result.Count(m => m.Value == res)}");
+                        }
+                    }
+
+                    var isAsync = IsAsyncSomewhere(textFile);
+
+                    if (isAsync)
+                    {
+                        if (!somethingFound)
+                        {
+                            Console.WriteLine($"{file}");
+                        }
+
+                        somethingFound = true;
+                        Console.WriteLine("  Contains some async potentially");
+                    }
+
+                    var isLinq = IsLinq(textFile);
+
+                    if (isLinq)
+                    {
+                        if (!somethingFound)
+                        {
+                            Console.WriteLine($"{file}");
+                        }
+
+                        somethingFound = true;
+                        Console.WriteLine("  Linq found");
+                    }
+
+                    var isUnitsNet = IsUnitsNet(textFile);
+
+                    if (isUnitsNet)
+                    {
+                        if (!somethingFound)
+                        {
+                            Console.WriteLine($"{file}");
+                        }
+
+                        _totalNumberUnitsNet++;
+                        somethingFound = true;
+                        Console.WriteLine("  Contains UnitsNet:");
+
+                        var units = FindUntis(textFile);
+                        foreach (var unit in units)
+                        {
+                            Console.WriteLine($"    {unit}");
+                        }
+
+                        _unitsUsed.AddRange(units);
+                    }
+
+                    if (somethingFound)
+                    {
+                        _totalFilesWithPotentialIssues++;
+                    }
+
+                    _totalFilesAnalyzed++;
+                    issuesInDirectory |= somethingFound;
+                }
+
+            }
+
+            foreach (var dir in Directory.GetDirectories(directory))
+            {
+                if ((dir.EndsWith("samples")) || dir.EndsWith("tests"))
+                {
+                    continue;
+                }
+
+                var (potentialIssue, transfo) = ProcessAllFiles(dir, false);
+                issuesInDirectory |= potentialIssue;
+                transformationNeeded |= transfo;
+
+                if (isRoot)
+                {
+                    _totalBindings++;
+
+                    if (potentialIssue)
+                    {
+                        _totalBindingsWithPotentialIssues++;
+                    }
+                    else if (transformationNeeded)
+                    {
+                        _totalTransformationNeeded++;
+                    }
+                }
+            }
+
+            return (issuesInDirectory, transformationNeeded);
+        }
+
+        private static MatchCollection SearchForSpanListT(string textFile)
+        {
+
+            string pattern = @"\w+<\w*>";
+            var result = Regex.Matches(textFile, pattern);
+            return result;
+
+        }
+
+        private static bool IsAsyncSomewhere(string textFile)
+        {
+            bool contains = textFile.Contains(" async ");
+            contains |= textFile.Contains("await ");
+            return contains;
+        }
+
+        private static bool IsLinq(string textfile)
+        {
+            return textfile.Contains("using System.Linq;");
+        }
+
+        private static bool IsUnitsNet(string textfile)
+        {
+            return textfile.Contains("using UnitsNet;");
+        }
+
+        private static List<string> FindUntis(string textfile)
+        {
+            List<string> units = new List<string>();
+            // We need to find patterns where there are possibly one of the Unit.
+            // They are either in a property, either in a function
+            // There muyst be a private or public or internal or protected in front in the same line and spaces before and after
+            // Typical example:
+            // public virtual RelativeHumidity Humidity
+            // And we have to do it for all units
+            foreach (var unit in _units)
+            {
+                string pattern = $"\\s((public)|(protected)|(internal)|(private))\\w*( {unit} )";
+                var result = Regex.Matches(textfile, pattern);
+                if (result.Count > 0)
+                {
+                    units.Add(unit);
+                }
+            }
+            return units.Distinct().ToList();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This repo is a prototype repository to explore how to better integrate [nanoFramework](https://github.com/nanoframework/Home) and [.NET IoT](https://github.com/dotnet/iot).
 
 In the repo:
+
 - Span
   - SpanByte which has been [implemented in nanoFramework](https://github.com/nanoframework/lib-CoreLibrary/blob/develop/nanoFramework.CoreLibrary/System/SpanByte.cs) which will be used to automatically generate Span<T>to SpanT
 - BinaryPrimitives implementation with SpanByte and byte[]. Note: the one which will be used it the SpanByte one. the byte[] was early work.
@@ -18,6 +19,31 @@ In the repo:
 - System.Device.Spi as a simple implementation in native code using the exisitng nanoFramework one. To be ported as well natively to nano
 - System.Device.Pwm as a simple implementation in native code using the exisitng nanoFramework one. To be ported as well natively to nano
 - TransforSpanList to transform automatically Span<T> and List<T> to SpanT and ListT
+
+## Static code analyzer
+
+A simple, brute force analyzer can help finding those elements:
+
+- Presence of Generics
+- UnitsNet and which units
+- Async, Linq
+
+so far the list of used Units is:
+
+- ElectricPotential
+- Temperature
+- Ratio
+- RelativeHumidity
+- Illuminance
+- Length
+- Pressure
+- Duration
+- ElectricResistance
+- ElectricCurrent
+- VolumeConcentration
+- Power
+- Frequency
+- ElectricPotentialDc
 
 ## Path to automate automatic creation of nanFramework compatible bindings from .NET IoT code
 
@@ -50,10 +76,13 @@ Notes for Bmxx80:
 
 - [x] Add the various Convert. missing in nanoFramework. More to come for sure but all very quick to do
 - [x] Work on migratin of Windows.Devices to System.Device, see [issue](https://github.com/nanoframework/Home/issues/620). Spi and Pwm missing
-- [X] Open issue on UnitsNet for autogeration of simple units nugets for nanoFramework based on the core units. See https://github.com/angularsen/UnitsNet/issues/836
+- [X] Open issue on UnitsNet for autogeration of simple units nugets for nanoFramework based on the core units. See https://github.com/angularsen/UnitsNet/issues/836. A PR has been raised, closed and will reopen, so focus can be placed on the main units.
 - [ ] Find a way o automatically replace Linq or change the policy and replace the few tens of lines in the current bindings => need to adjust the .NET IoT code to get rid of those lines
 - [ ] Math.XXX [issues](https://github.com/nanoframework/Home/issues/642), a solution will be found.
 - [ ] Add more devices to check performance and migration path, discover new missing elements from System that may be used by other bindings
 - [ ] Create specific policies to make a binding nanoFramework compatible => work started
 - [ ] Work on automated code to transform original code and create specific nanoFramework compatible project + work on CI/CD => work started
 - [x] Performance check on sensitive sensors like DHT => DHT won't work on some devices like ESP32. But there are ways to have those devices working but not with GpioController => out of scope for automatic generation
+- [ ] Define the documentation and steps needed for manual adjustments
+- [ ] Find a smart way to migrate samples
+- [x] ILogger => now compatible and in place in [nano](https://github.com/nanoframework/nanoFramework.Logging)
